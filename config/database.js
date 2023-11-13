94aa5abac7469012e91e0d403a0716189b42d2dd
Riya Madhur <riyamadhur@gmail.com>
2023-11-13 19:17:22 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+// here we connect the odm(object document maper) mongoose to the dbms mongodb. 
+mongoose.connect(process.env.MONGO_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+
+
+const db = mongoose.connection; // db store the connection
+
+// cheacking the connection
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("DATABASE connection is Established");
+});
+
+// exporting the connection.
+module.exports = db;
